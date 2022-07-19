17d2807a799b182d42406697894351e6c2414dc2
YleWang <109201432+YleWang@users.noreply.github.com>
2022-07-19 15:34:25 +0800

    Update 组成3位数.py

--- 组成3位数.py
+++ 组成3位数.py
@@ -1,12 +1,12 @@
 import random
 list=[]
-len(list)<64 == True
-while (len(list)<64):
+len(list)<24 == True
+while (len(list)<24):
     a=random.randint(1,4)
     b=random.randint(1,4)
     c=random.randint(1,4)
     d=(a,b,c)
-    if d in list:
+    if d in list or a==b or a==c or b==c:
         pass
     else:
         list.append(d)
